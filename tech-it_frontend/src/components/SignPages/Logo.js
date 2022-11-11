ac07cd65c28c4a25adc91181304627c90fbb1a57
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-11 16:37:27 +0200

    Creation<> Log Component: Logo

---
+++ tech-it_frontend/src/components/SignPages/Logo.js
@@ -0,0 +1,14 @@
+const Logo = ({ submit }) => {
+
+  return (
+    <div className="absolute top-[0px] left-[0px] w-1/2 h-screen overflow-hidden">
+    <img
+      className="absolute top-[180px] left-[102px] w-[515.51px] h-[664px] object-cover"
+      alt=""
+      src="../techitlogos-transparent@2x.png"
+    />
+  </div>
+  );
+};
+
+export default Logo;
