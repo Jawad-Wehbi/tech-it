85ea0ab637756a126d1143dc259e092b1030a3dc
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-13 12:24:22 +0200

    Update<> input.js

--- tech-it_frontend/src/components/SignPages/Input.js
+++ tech-it_frontend/src/components/SignPages/Input.js
@@ -1,7 +1,7 @@
 const Input = ({ placeholder = "Placeholder" }) => {
   return (
     <input
-      className="[border:1px_solid_#eaeaea] bg-gray-100 relative rounded-[5px] box-border w-[380px] h-[45px] shrink-0 cursor-text"
+      className="[border:1px_solid_#eaeaea] bg-gray-100 relative rounded-[5px]  indent-2 box-border w-[380px] h-[45px] shrink-0 cursor-text"
       type="text"
       placeholder={placeholder}
       required
