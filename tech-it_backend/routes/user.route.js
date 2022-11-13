a5823a03a38164e9bc5c605ee5e4f8be66f21b15
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-13 15:21:47 +0200

    Implementation<> Routes: user.route

---
+++ tech-it_backend/routes/user.route.js
@@ -0,0 +1,5 @@
+// const router = require('express').Router();
+// const user = require('../controllers/user.controller');
+
+// // getallmembers
+// router.get('/getallmembers', user.getAllMembers);
