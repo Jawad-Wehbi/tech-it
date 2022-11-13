5ec7bb64e6b534a5aa0dfb02486ce14632fdb9a4
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-13 14:30:08 +0200

    Implementation<> Dashboard Component: CreateTest

---
+++ tech-it_frontend/src/components/Dashboard/createTest.js
@@ -0,0 +1,32 @@
+import { Grid, Container, Paper, Box, Typography, Stack, Button } from '@mui/material';
+import * as React from 'react';
+
+const CreateTest = () => {
+	return (
+		<>
+			<Container maxWidth={false} disableGutters>
+				<Box sx={{ mt: 6, ml: 6 }}>
+					<Typography variant="h3">Test in different topics!</Typography>
+
+					<Stack
+						spacing={2}
+						direction="row"
+						sx={{ mt: 4, justifyContent: 'space-between', alignItems: 'center', mr: 20 }}>
+						<Typography variant="h5">
+							Create your test template in different topics with an endless list of questions.
+						</Typography>
+
+						<Button
+							variant="contained"
+							style={{ width: 300, height: 40 }}
+							sx={{ mr: 5, bgcolor: '#9747FF' }}>
+							Create
+						</Button>
+					</Stack>
+				</Box>
+			</Container>
+		</>
+	);
+};
+
+export default CreateTest;
