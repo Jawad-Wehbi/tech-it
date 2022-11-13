8f43afab62704e033d1182b6ed68fa812a094c62
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-13 12:04:56 +0200

    Update<> App.js

--- tech-it_frontend/src/App.js
+++ tech-it_frontend/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
+import CompanySignUp from './pages/CompanySignUp';
+import TeamMembers from './pages/TeamMembers';
+
+import SignIn from './pages/SignIn';
+import NavigationBar from './components/Utilities/NavigationBar';
+import Footer from './components/Utilities/Footer';
+import { ProtectedRoute } from './helpers/PrivateRoute';
+import NoPage from './pages/NoPage';
+import Layout from './layout/Layout';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Dashboard from './pages/Dashboard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				{/* <Route exact path="/" element={<ProtectedRoute />}> */}
+				{/* <Route exact path="/navigation-bar" element={<NavigationBar />} /> */}
+				<Route
+					exact
+					path="/"
+					element={
+						<Layout>
+							<Dashboard />
+						</Layout>
+					}
+				/>
+
+				<Route
+					exact
+					path="/TeamMembers"
+					element={
+						<Layout>
+							<TeamMembers />
+						</Layout>
+					}
+				/>
+
+				<Route path="*" element={<NoPage />} />
+				{/* </Route> */}
+
+				{/* Non-Protected Paths */}
+				<Route exact path="/sign-up" element={<CompanySignUp />} />
+				<Route exact path="/sign-in" element={<SignIn />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
