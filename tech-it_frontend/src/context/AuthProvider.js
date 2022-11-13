a41c4aa3725fef4c1fd3216b2b4bec118f524c7c
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-13 14:44:20 +0200

    Implementation<> AuthProvider

---
+++ tech-it_frontend/src/context/AuthProvider.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from "react";
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  let [profile, setProfile] = useState({});
+	let [tokenData, setTokenData] = useState({});
+	let [tokens, setTokens] = useState({});
+
+    return (
+        <AuthContext.Provider value={{ profile, setProfile, tokenData, setTokenData, tokens, setTokens }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => React.useContext(AuthContext);
