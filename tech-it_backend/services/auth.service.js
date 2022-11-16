3ed4cb66307ca06b77eba75541ac249552a02df5
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-16 16:04:03 +0200

    Fixation<> auth.service: Log-in

--- tech-it_backend/services/auth.service.js
+++ tech-it_backend/services/auth.service.js
@@ -40,8 +40,26 @@ const register = async (data) => {
   });
 };
 
-
+const login = async (data) => {
+  console.log("------> data :>> ", data);
+  const { email, password } = data;
+  const user = await prisma.Users.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (!user) {
+    throw createError.NotFound("User not registered");
+  }
+  const checkPassword = bcrypt.compareSync(password, user.password);
+  if (!checkPassword)
+    throw createError.Unauthorized("Email address or password not valid");
+  delete user.password;
+  const accessToken = await jwt.signAccessToken(user);
+  return { ...user, accessToken };
+};
 
 module.exports = {
   register,
+  login,
 };
