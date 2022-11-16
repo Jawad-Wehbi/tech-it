fc44cdd7741ac79662eb0fe12fbb1a68932e1a4f
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-16 17:48:45 +0200

    Implementation<>company.controller: GetQuestionsBy

--- tech-it_backend/controllers/company.controller.js
+++ tech-it_backend/controllers/company.controller.js
@@ -18,8 +18,22 @@ const getAlltests = async (req, res, next) => {
     res.status(400).json(formatFatalError(err));
   }
 };
+const getQuestionsByTopic = async (req, res, next) => {
+  try {
+    const questions = await company.getQuestionsByTopic(req.body);
+    res.status(200).json({
+      message: "Questions shown successfully",
+      data: questions,
+    });
+  } catch (err) {
+    console.log("e :>> ", err);
+    res.status(400).json(formatFatalError(err));
+  }
+};
 
 module.exports={
-  getAlltests,
+  getAlltests,  
+  getQuestionsByTopic,
+
 
 } 
