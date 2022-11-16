5ac8c5617c0d84e54cee63286b0e8b8445103de0
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-16 16:06:39 +0200

    Fixation<> auth.route

--- tech-it_backend/routes/auth.route.js
+++ tech-it_backend/routes/auth.route.js
@@ -5,6 +5,4 @@ const auth = require('../middlewares/auth.middleware');
 router.post('/register', user.register);
 // login
 router.post('/login', user.login);
-// all users
-router.get('/', auth, user.all);
 module.exports = router;
