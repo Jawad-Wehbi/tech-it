6229fc4332d5e6115c4c707c2c6c2a09cd856fc9
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-16 13:24:25 +0200

    Update<> Index.js

--- tech-it_backend/index.js
+++ tech-it_backend/index.js
@@ -1,18 +1,20 @@
-const express = require('express');
-require('dotenv').config();
-require('./config/db.config');
-require('@prisma/client');
+const express = require("express");
+require("dotenv").config();
+require("./config/db.config");
+require("@prisma/client");
+const auth = require("./middlewares/auth.middleware");
 
 const app = express();
 app.use(express.json());
 
-
-const authRoutes = require('./routes/auth.route');
-app.use('/auth', authRoutes)
-// const userRoutes = require('./routes/user.route');
-// app.use('/user', userRoutes)
+const authRoutes = require("./routes/auth.route");
+app.use("/auth", authRoutes);
+const userRoutes = require("./routes/user.route");
+app.use("/user", auth, userRoutes);
+const companyRoutes = require("./routes/company.route");
+app.use("/company", auth, companyRoutes);
 
 app.listen(process.env.PORT, (err) => {
-    if(err) throw err;
-    console.log(`server running on port ${process.env.PORT}`)
-  })
+  if (err) throw err;
+  console.log(`server running on port ${process.env.PORT}`);
+});
