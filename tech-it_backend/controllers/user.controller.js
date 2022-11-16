09d6396660d658442ba4feab20c51cb7130fd7db
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-16 16:16:09 +0200

    Implementation<>user.controller: GetQuestionByTest

--- tech-it_backend/controllers/user.controller.js
+++ tech-it_backend/controllers/user.controller.js
@@ -1,19 +1,30 @@
-// const express, {request, response} = require("express")
-const db = require("../config/db.config");
+const { questions } = require("../config/db.config");
+const user = require("../services/user.service");
 
 const getAllMembers = async (req, res) => {
   try {
-    const assignees = await db.assignees.findMany({
-      where: {
-        user_type: "Employee",
-      },
-    });
+    const assignees = await db.users.findMany();
     return res.json(assignees);
   } catch (e) {
     res.send(e.message);
   }
 };
 
+
+const getQuestionsByTest = async (req, res, next) => {
+  try {
+    const id = req.body.id;
+    const test = await user.getQuestionsByTest(id);
+    res.status(200).json({
+      message: "Test info shown successfully",
+      data: test,
+    });
+  } catch (err) {
+    console.log("e :>> ", err);
+    res.status(400).json(formatFatalError(err));
+  }
+};
 module.exports={
-  getAllMembers
-}
+  getAllMembers,
+  getQuestionsByTest
+} 
