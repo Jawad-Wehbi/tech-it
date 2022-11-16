3173b877757600e11fee66297762ba28359e13f5
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-16 17:02:26 +0200

    Implementation<>company.route: GetQuestionsByTopic

--- tech-it_backend/routes/company.route.js
+++ tech-it_backend/routes/company.route.js
@@ -3,6 +3,8 @@ const company = require('../controllers/company.controller');
 
 // getalltests
 router.get('/getalltests', company.getAlltests);
+// getQuestionsByTopic
+router.post('/getQuestionsByTopic', company.getQuestionsByTopic);
 
 
 module.exports = router;
