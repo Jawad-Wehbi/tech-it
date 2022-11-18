e1ddc66b8aeaadcda3dbdc2a2684d2f7030f47e9
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-18 09:44:25 +0200

    Addition<> Colors

--- tech-it_frontend/src/values/colors.js
+++ tech-it_frontend/src/values/colors.js
@@ -2,5 +2,7 @@ export const Colors = {
 	button_purple: '#5A4AE3',
 	header_purple: '#5A4AE3',
 	black: '#222222',
-	purple: '#5A4AE3'
+	purple: '#5A4AE3',
+	white: '#ffffff',
+	question_bg: '#E9E9E9'
 };
