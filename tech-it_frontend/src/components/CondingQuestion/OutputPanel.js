75583800be37d2b4c583615dd7abde135b2a9b1c
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-18 09:46:34 +0200

    Implementation<> CodingQ. Component: Output Panel

---
+++ tech-it_frontend/src/components/CondingQuestion/OutputPanel.js
@@ -0,0 +1,16 @@
+import { Container, Paper } from '@mui/material';
+import * as React from 'react';
+import { Colors } from '../../values/colors';
+
+
+const OutputPanel = () => {
+	return (
+		<Container>
+			<Paper sx={{ height: '52vh', padding: '10px', bgcolor: Colors.question_bg }}>
+
+      </Paper>
+		</Container>
+	);
+};
+
+export default OutputPanel;
