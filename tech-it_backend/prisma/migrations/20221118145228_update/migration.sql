ed0eda977084ae19f1f0cdceea77fd98e5f24868
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-18 17:03:15 +0200

    Update<> Database: Clean-up

---
+++ tech-it_backend/prisma/migrations/20221118145228_update/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `output` on the `mcq_selected_choices` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `mcq_selected_choices` DROP COLUMN `output`;
