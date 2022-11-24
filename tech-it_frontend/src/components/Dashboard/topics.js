44757fae80c6ee6693308eb5303870367ec618da
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-24 12:04:49 +0200

    Update<> Dashboard components: Topics

--- tech-it_frontend/src/components/Dashboard/topics.js
+++ tech-it_frontend/src/components/Dashboard/topics.js
@@ -1,8 +1,16 @@
 import { Container, Paper, Typography, Button, Grid, Stack } from '@mui/material';
 import { Box } from '@mui/system';
-import React, { useState } from 'react';
+import React, {useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Topics = () => {
+	const navigate = useNavigate();
+
+	const handleSubmit = (topic, navigate) => {
+		console.log('Topic :>> ', topic);
+		navigate('/Tests', { state: { params: topic } });
+	};
+
 	return (
 		<>
 			<Container maxWidth={false} disableGutters>
@@ -17,62 +25,82 @@ const Topics = () => {
 							boxShadow: 2
 						}
 					}}>
-					<Paper sx={{pb: 6}}>
+					<Paper sx={{ pb: 6 }}>
 						<Stack justifyContent="center" alignItems="center">
 							<Typography
 								variant="h4"
-								sx={{ justifyContent: 'center', alignItems: 'center', m:2}}>
+								sx={{ justifyContent: 'center', alignItems: 'center', m: 2 }}>
 								Topics
 							</Typography>
-							<Paper sx={{ height: '100%', width: '80%', flexDirection: 'column', bgcolor: '#F4F4F4', py: 3 }}>
+							<Paper
+								sx={{
+									height: '100%',
+									width: '80%',
+									flexDirection: 'column',
+									bgcolor: '#F4F4F4',
+									py: 3
+								}}>
 								<Stack direction="row" spacing={8} justifyContent="center" alignItems="center">
-									<Button variant="contained" size="medium">
+									<Button
+										variant="contained"
+										size="medium"
+										onClick={()=> handleSubmit("PYTHON",navigate)}>
 										<Typography variant="subtitle1">Python</Typography>
 									</Button>
 
-									<Button variant="contained" size="medium">
+									<Button variant="contained" size="medium"
+										onClick={()=> handleSubmit('DATA_STRUCTURE', navigate)}>
 										<Typography variant="subtitle1">Data Structure</Typography>
 									</Button>
 
-									<Button variant="contained" size="medium">
+									<Button onClick={() => handleSubmit('C', navigate)} variant="contained" size="medium">
 										<Typography variant="subtitle1">C</Typography>
 									</Button>
 								</Stack>
 
 								<Stack direction="row" spacing={8} justifyContent="center" alignItems="center">
-									<Button variant="contained" size="medium">
+									<Button 
+										onClick={() => handleSubmit('JAVA', navigate)} variant="contained" size="medium">
 										<Typography variant="subtitle1">Java</Typography>
 									</Button>
-									<Button variant="contained" size="medium">
+									<Button 
+										onClick={() => handleSubmit('JAVASCRIPT', navigate)} variant="contained" size="medium">
 										<Typography variant="subtitle1">JavaScript</Typography>
 									</Button>
-									<Button variant="contained" size="medium">
+									<Button 
+										onClick={() => handleSubmit('SQL', navigate)} variant="contained" size="medium">
 										<Typography variant="subtitle1">SQL</Typography>
 									</Button>
 								</Stack>
 
 								<Stack direction="row" spacing={8} justifyContent="center" alignItems="center">
-									<Button variant="contained" size="medium">
+									<Button 
+										onClick={() => handleSubmit('DATABASES', navigate)} variant="contained" size="medium">
 										<Typography variant="subtitle1">Databases</Typography>
 									</Button>
 
-									<Button variant="contained" size="medium">
+									<Button 
+										onClick={() => handleSubmit('C#', navigate)} variant="contained" size="medium">
 										<Typography variant="subtitle1">C#</Typography>
 									</Button>
-									<Button variant="contained" size="medium">
+									<Button 
+										onClick={() => handleSubmit('PHP', navigate)} variant="contained" size="medium">
 										<Typography variant="subtitle1">PHP</Typography>
 									</Button>
 								</Stack>
 
 								<Stack direction="row" spacing={8} justifyContent="center" alignItems="center">
-									<Button variant="contained" size="medium">
+									<Button 
+										onClick={() => handleSubmit('ALGORITHMS', navigate)} variant="contained" size="medium">
 										<Typography variant="subtitle1">Algorithms</Typography>
 									</Button>
-									<Button variant="contained" size="medium">
+									<Button 
+										onClick={() => handleSubmit('RUBY', navigate)} variant="contained" size="medium">
 										<Typography variant="subtitle1">Ruby</Typography>
 									</Button>
 
-									<Button variant="contained" size="medium">
+									<Button 
+										onClick={() => handleSubmit('COMPANY_TESTS', navigate)} variant="contained" size="medium">
 										<Typography variant="subtitle1">Company Tests</Typography>
 									</Button>
 								</Stack>
@@ -85,72 +113,5 @@ const Topics = () => {
 	);
 };
 
-{
-	/* <Grid item xs={3} md={3}>
-										<Button variant="contained" size="medium">
-											Python
-										</Button>
-									</Grid>
-
-									<Grid item xs={3} md={3}>
-										<Button variant="contained" size="medium">
-											Data Structure
-										</Button>
-									</Grid>
-
-									<Grid item xs={3} md={3}>
-										<Button variant="contained" size="medium">
-											C
-										</Button>
-									</Grid>
-
-									<Grid item xs={3} md={3}>
-										<Button variant="contained" size="medium">
-											Java
-										</Button>
-									</Grid>
-
-									<Grid item xs={3} md={3}>
-										<Button variant="contained" size="medium">
-											JavaScript
-										</Button>
-									</Grid>
-
-									<Grid item xs={3} md={3}>
-										<Button variant="contained" size="medium">
-											SQL
-										</Button>
-									</Grid>
-
-									<Grid item xs={3} md={3}>
-										<Button variant="contained" size="medium">
-											Databases
-										</Button>
-									</Grid>
-
-									<Grid item xs={3} md={3}>
-										<Button variant="contained" size="medium">
-											PHP
-										</Button>
-									</Grid>
-
-									<Grid item xs={3} md={3}>
-										<Button variant="contained" size="medium">
-											Algorithms
-										</Button>
-									</Grid>
-
-									<Grid item xs={3} md={3}>
-										<Button variant="contained" size="medium">
-											Ruby
-										</Button>
-									</Grid>
-
-									<Grid item xs={3} md={3}>
-										<Button variant="contained" size="medium">
-											Company Tests
-										</Button>
-									</Grid> */
-}
 
 export default Topics;
