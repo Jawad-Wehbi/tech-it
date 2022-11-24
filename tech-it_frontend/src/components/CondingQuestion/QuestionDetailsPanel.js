ab0a132d5bfabc5b9d8109f51aaad8f4f529409b
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-24 12:03:00 +0200

    Update<>Coding Question component:
    Question Details Panel

--- tech-it_frontend/src/components/CondingQuestion/QuestionDetailsPanel.js
+++ tech-it_frontend/src/components/CondingQuestion/QuestionDetailsPanel.js
@@ -2,32 +2,32 @@ import { Container, Paper, ListItemText, Typography } from '@mui/material';
 import * as React from 'react';
 import { Colors } from '../../values/colors';
 const TestDetailsPanel = (props) => {
-  
-	const {TestTitle="Test Title", TestDetails="Test details" } = props;
+	const { TestTitle, TestDetails } = props;
 	return (
-		<Container >
-						<Paper sx={{ height: '12vh', padding: '10px',marginBottom: "2vh", bgcolor: Colors.question_bg }}>
-							<ListItemText
-								primary={
-									<React.Fragment>
-										<Typography sx={{ display: 'inline' }} variant="h5">
-											{TestTitle}
-										</Typography>
-									</React.Fragment>
-								}
-								secondary={
-									<React.Fragment>
-										<Typography
-											sx={{ display: 'inline' }}
-											component="span"
-											variant="body2"
-											color="text.primary">
-											{TestDetails}
-										</Typography>
-									</React.Fragment>
-								}
-							/>
-						</Paper>
+		<Container disableGutters>
+			<Paper
+				sx={{ height: '12vh', padding: '10px', marginBottom: '2vh', bgcolor: Colors.question_bg }}>
+				<ListItemText
+					primary={
+						<React.Fragment>
+							<Typography sx={{ display: 'inline' }} variant="h5">
+								{TestTitle}
+							</Typography>
+						</React.Fragment>
+					}
+					secondary={
+						<React.Fragment>
+							<Typography
+								sx={{ display: 'inline' }}
+								component="span"
+								variant="body1"
+								color="text.primary">
+								{TestDetails}
+							</Typography>
+						</React.Fragment>
+					}
+				/>
+			</Paper>
 		</Container>
 	);
 };
