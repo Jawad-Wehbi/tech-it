40cec62503f8de7ff6b760b4b1079aa4d84ef729
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-25 23:15:45 +0200

    Update<> Team Members Component:
    Header

--- tech-it_frontend/src/components/TeamMembers/Header.js
+++ tech-it_frontend/src/components/TeamMembers/Header.js
@@ -1,7 +1,43 @@
-import { Grid, Container, Paper, Box, Stack, Toolbar, Button, Typography } from '@mui/material';
-import * as React from 'react';
+import {
+	Grid,
+	Container,
+	Paper,
+	Box,
+	Stack,
+	Toolbar,
+	Button,
+	Typography,
+	Alert
+} from '@mui/material';
+import React, { useRef, useState } from 'react';
+import AddMemberForm from './AddMember';
+import AssignTestDailog from './AssignTestDailog';
+import Slide from '@mui/material/Slide';
+import Snackbar from '@mui/material/Snackbar';
 
-const Header = () => {
+const Header = (props) => {
+	const isCompany = true;
+
+	const { addMember } = props;
+	const childRef = useRef(null);
+	const addMemberRef = useRef(null);
+
+	const [open, setOpen] = useState(false);
+	const [transition, setTransition] = useState(undefined);
+
+	function TransitionRight(props) {
+		return <Slide {...props} direction="right" />;
+	}
+
+	const handleClick = (Transition) => {
+		setTransition(() => Transition);
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
 	return (
 		<>
 			<Container maxWidth={false} disableGutters>
@@ -36,22 +72,47 @@ const Header = () => {
 										</Toolbar>
 									</Box>
 
-									<Box>
+									<Box spacing={2}>
 										<Button
 											variant="contained"
 											style={{ width: 300, height: 40 }}
-											sx={{ mr: 5, bgcolor: '#9747FF' }}>
-											ADD MEMBER
+											sx={{ mr: 5, bgcolor: '#9747FF' }}
+											onClick={() => {
+												// childRef.current.handleClickOpen(true);
+												console.log('clicked :>> ');
+												handleClick(TransitionRight);
+											}}>
+											Assign Tests
 										</Button>
+										<AssignTestDailog ref={childRef} />
+										<Snackbar
+											open={open}
+											severity="success"
+											onClose={handleClose}
+											TransitionComponent={transition}
+											message="I love snacks"
+											key={transition ? transition.name : ''}>
+											<Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+												Interesting! Test Assigned
+											</Alert>
+										</Snackbar>
+										{isCompany && (
+											<Button
+												variant="contained"
+												style={{ width: 300, height: 40 }}
+												sx={{ mr: 5, bgcolor: '#9747FF' }}
+												onClick={() => {
+													addMemberRef.current.handleClickOpen(true);
+												}}>
+												ADD MEMBER
+											</Button>
+										)}
+										<AddMemberForm ref={addMemberRef} addMember={addMember} />
 									</Box>
 								</Stack>
 							</Paper>
 						</Container>
 					</Grid>
-
-					<Grid item xs={12} md={12}></Grid>
-
-					<Grid item xs={12} md={12} sx={{ mt: 5 }}></Grid>
 				</Grid>
 			</Container>
 		</>
