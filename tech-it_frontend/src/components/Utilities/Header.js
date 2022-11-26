55388ff91bf2fbd1a1a7e968bf148b73ce2f19c2
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-26 04:30:53 +0200

    Implementation<>Utilities component: Header

---
+++ tech-it_frontend/src/components/Utilities/Header.js
@@ -0,0 +1,53 @@
+import { Grid, Container, Paper, Box, Stack, Toolbar, Typography } from '@mui/material';
+import * as React from 'react';
+import { Colors } from '../../values/colors';
+
+const Header = (props) => {
+	const { Header="Header" } = props;
+	return (
+		<>
+			<Container maxWidth={false} disableGutters>
+				<Grid container sx={{ justifyContent: 'center' }}>
+					<Grid item xs={10} md={10}>
+						<Container maxWidth={false} sx={{ mt: 4 }}>
+							<Paper
+								variant="outlined"
+								sx={{
+									bgcolor: Colors.black,
+									borderRadius: 2,
+									boxShadow: 2
+								}}>
+								<Stack
+									direction="row"
+									spacing={1}
+									sx={{
+										display: 'flex',
+										justifyContent: 'space-between',
+										alignItems: 'center'
+									}}>
+									<Box
+										sx={{
+											display: 'flex',
+											justifyContent: 'flex-start'
+										}}>
+										<Toolbar>
+											<Typography variant="h6" align="left" color="#ffffff">
+												{Header}
+											</Typography>
+										</Toolbar>
+									</Box>
+								</Stack>
+							</Paper>
+						</Container>
+					</Grid>
+
+					<Grid item xs={12} md={12}></Grid>
+
+					<Grid item xs={12} md={12} sx={{ mt: 5 }}></Grid>
+				</Grid>
+			</Container>
+		</>
+	);
+};
+
+export default Header;
