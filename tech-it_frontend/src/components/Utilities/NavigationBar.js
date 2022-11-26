e89943a9b5d043b56e9b4d91ee87749adf73d7db
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-26 04:31:22 +0200

    Update<>Utilities component:
    Navigation Bar

--- tech-it_frontend/src/components/Utilities/NavigationBar.js
+++ tech-it_frontend/src/components/Utilities/NavigationBar.js
@@ -1,5 +1,5 @@
-import { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
+import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined';
 
 const NavigationBar = ({ logo, companyName = 'Company Name' }) => {
 	const navigate = useNavigate();
@@ -12,49 +12,43 @@ const NavigationBar = ({ logo, companyName = 'Company Name' }) => {
 		navigate('/TeamMembers');
 	};
 
-	const onFrameLink4Click = useCallback(() => {
-		// Please sync "Assessments Done" to the project
-	}, []);
+	const onFrameLink4Click = () => {
+		navigate('/AssessmentsDone');
+	};
 
 	return (
 		<div className="relative bg-indigo-100 w-screen h-[130px] shrink-0 overflow-hidden text-center text-lg text-white font-roboto">
 			<div className="absolute top-[45px] left-1/3 w-1/2 flex flex-row items-center justify-between">
-				<a
-					href=""
+				<button
 					className="[text-decoration:none] relative w-1/4 h-[41px] top-[calc(50%_-_20.5px)] flex flex-row p-[11px_25px] box-border items-center cursor-pointer justify-center text-[inherit]"
 					onClick={onFrameLink2Click}>
 					<div className="relative inline-block">Dashboard</div>
-				</a>
-				<a
-					
+				</button>
+				<button
 					className="[text-decoration:none] relative w-1/4 h-[41px] top-[calc(50%_-_20.5px)] flex flex-row p-[1px_2px] box-border items-center cursor-pointer justify-center text-[inherit]"
 					onClick={onFrameLink3Click}>
 					<div className="relative inline-block">Team Members</div>
-				</a>
+				</button>
 
-				<a
-					href="AssessmentsDone"
+				<button
 					className="[text-decoration:none] top-[calc(50%_-_20.5px)] box-border items-center justify-center flex flex-row p-[11px_25px] relative w-1/4 h-[41px] shrink-0 cursor-pointer text-[inherit]"
 					onClick={onFrameLink4Click}>
 					<div className="relative inline-block">Assessments</div>
-				</a>
+				</button>
 			</div>
 			<div className="absolute top-[47px] right-[2%] w-[160px] flex flex-row items-center justify-between">
 				<div className="relative w-[125px] h-[44px] shrink-0">
-					<div className="absolute h-[81.82%] w-[62.4%] top-[9.09%] left-[24%] font-semibold flex items-center justify-center">
-						{companyName}
+					<div className="absolute h-[81.82%] w-[62.4%] top-[20.09%] ml-1 left-[24%] font-semibold flex items-center justify-center">
+						SE Factory
 					</div>
-					<img
-						className="absolute h-[54.55%] w-[19.2%] top-[22.73%] right-[76%] bottom-[22.73%] left-[4.8%] max-w-full overflow-hidden max-h-full"
+					{/* <img
+						className="absolute h-[70.55%] w-[30.2%] top-[22.73%] right-[76%] bottom-[22.73%] max-w-full overflow-hidden max-h-full"
 						alt=""
-						src={logo}
-					/>
+						src="default-profile.png"
+					/> */}
 				</div>
-				<img
-					className="relative w-[36px] h-[36px] shrink-0 overflow-hidden"
-					alt=""
-					src="../claritynotificationsolid.svg"
-				/>
+				<NotificationsActiveOutlinedIcon fontSize='large' />
+				
 			</div>
 			<div className="absolute top-[15px] left-[35px] w-[292px] flex flex-row items-center justify-start text-3xl">
 				<img
