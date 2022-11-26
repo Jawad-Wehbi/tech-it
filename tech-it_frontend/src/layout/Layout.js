5ddd5d7c6fdf348eb25a7d07ac9f60099d19da97
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2022-11-26 04:33:13 +0200

    Clean up<> Layout

--- tech-it_frontend/src/layout/Layout.js
+++ tech-it_frontend/src/layout/Layout.js
@@ -1,31 +1,14 @@
 import * as React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { Box, Paper } from '@mui/material';
-import Toolbar from '@mui/material/Toolbar';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
+import { Box } from '@mui/material';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import MenuIcon from '@mui/icons-material/Menu';
-import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import LogoutIcon from '@mui/icons-material/Logout';
-// import { MenuListItems } from './MenuListItems';
-// import AuthApi from '../helpers/auth';
-// import AppBar from './AppBar';
-// import Drawer from './Drawer';
-// import Copyright from './CopyRight';
-// import logo from '../assets/logo.png';
-import InputAdornment from '@mui/material/InputAdornment';
-import SearchIcon from '@mui/icons-material/Search';
-import TextField from '@mui/material/TextField';
 import NavigationBar from '../components/Utilities/NavigationBar';
 import Footer from '../components/Utilities/Footer';
 
-const Layout = ({ footer=true, children }) => {
+const Layout = (props) => {
+	const { children, footer = true, checked, onChange } = props;
 	const [open, setOpen] = React.useState(true);
+
 	const toggleDrawer = () => {
 		setOpen(!open);
 	};
@@ -57,7 +40,7 @@ const Layout = ({ footer=true, children }) => {
 					}}>
 					<Container maxWidth={false} disableGutters>
 						<Grid container>{children}</Grid>
-						{footer && <Footer sx={{ alignItems: 'flex-end' }} />}
+						{footer && <Footer style={{ alignItems: 'flex-end' }} checked={checked} onChange={onChange} />}
 					</Container>
 				</Box>
 				{/* </Grid> */}
